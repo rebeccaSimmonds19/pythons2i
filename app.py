6e8916eb65666a8ac656f38d423efead3e43478b
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-10-18 15:38:37 +0100

    sparkSession added for oshinko cluster

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import pyspark
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import mean, desc
 
-sparkSession = SparkSession.builder.master("local[*]") \
+sparkSession = SparkSession.builder.master("spark://test:7077") \
 .getOrCreate()
