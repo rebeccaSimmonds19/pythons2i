5fd8c42e8e08a1f1049fc34bf7f0d4fc1b956d9d
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-10-18 15:22:29 +0100

    sparkSession added for local spark

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('hello world')
+import pyspark
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.functions import mean, desc
+import plotly.plotly as py
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot,iplot
+init_notebook_mode(connected=True)
+
+sparkSession = SparkSession.builder.master("local[*]") \
+.config("spark.driver.extraClassPath","/opt/postgresql-42.1.4.jar") \
+.getOrCreate()
