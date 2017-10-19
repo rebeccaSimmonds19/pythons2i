078652cbe5408f55cdc49545816b82bbb443b30a
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-10-19 15:41:05 +0100

    add file for postgreql jar

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ import plotly.plotly as py
 
 sparkSession = SparkSession.builder.master("local[*]") \
 .config("spark.driver.extraClassPath","postgresql-42.1.4.jar") \
+.config("spark.jars", "postgresql-42.1.4.jar")
 .getOrCreate()
+
+sparkSession.sparkContext.addFile("postgresql-42.1.4.jar")
+
+spark
 
 import psycopg2
 conn = psycopg2.connect("host='172.17.0.3' port='5432' dbname='wineDb' user='username' password='password'")
