3db3f38baf27fd5631e7a90ac00d6cab9506edd9
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-10-19 16:16:43 +0100

    add the driver before the cluster is made

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from pyspark.sql.functions import mean, desc
 import plotly.plotly as py
 
 sparkSession = SparkSession.builder.master("local[*]") \
+._jsc.addJar("postgresql-42.1.4.jar") \
 .getOrCreate()
 
 #.config("spark.driver.extraClassPath","postgresql-42.1.4.jar") \
 #.config("spark.jars", "postgresql-42.1.4.jar") \
 #sparkSession.sparkContext.addFile("postgresql-42.1.4.jar")
-
-sparkSession._jsc.addJar("postgresql-42.1.4.jar")
 
 import psycopg2
 conn = psycopg2.connect("host='172.17.0.3' port='5432' dbname='wineDb' user='username' password='password'")
