b8bfb4f67158d7e632b8df2b5a939abbd56d43fe
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-10-19 13:24:37 +0100

    taken out notebook specific imports

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import pyspark
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import mean, desc
 import plotly.plotly as py
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot,iplot
-init_notebook_mode(connected=True)
 
 sparkSession = SparkSession.builder.master("local[*]") \
 .config("spark.driver.extraClassPath","/opt/postgresql-42.1.4.jar") \
@@ -43,4 +41,4 @@ data =  dict(type = 'choropleth',
 )
 layout = dict(geo = {'scope':'world'})
 choromap = go.Figure(data = [data],layout = layout)
-iplot(choromap)
+py(choromap)
