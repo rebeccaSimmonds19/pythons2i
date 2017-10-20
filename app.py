693850b2feb385943bfa5626959b57399d3ccfc2
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-10-20 15:08:21 +0100

    added in flask

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import mean, desc
 from plotly.offline import download_plotlyjs, plot
 from plotly.graph_objs import *
+from flask import app, make_response, render_template
+
 sparkSession = SparkSession.builder.master("local[*]") \
 .getOrCreate()
 
@@ -47,7 +49,6 @@ data =  dict(type = 'choropleth',
 layout = dict(geo = {'scope':'world'})
 choromap = dict(data=[data], layout=layout)
 index(choromap)
-from flask import app, make_response, render_template
 
 
 
