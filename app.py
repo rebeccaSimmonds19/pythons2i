d539c80c53533bb5c0c50e2e1218f60170eeb7a8
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-11-02 16:23:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,45 @@ app = Flask(__name__)
 
 @app.route('/')                                            
 def index():
-    return "Hello"
-@app.route('/map')
+    mport psycopg2
+    conn = psycopg2.connect("host='172.17.0.3' port='5432' dbname='wineDb' user='username' password='password'")
+    cur = conn.cursor()
+    #make table
+    f = open(r'/opt/app-root/src/wineData.csv', 'r')
+    cur.copy_from(f, "wine_reviews", sep=',')
+    conn.commit()
+    #cur.execute('create table wine_reviews(country VARCHAR, designation VARCHAR, points INT, price VARCHAR, province VARCHAR, region_1 VARCHAR, region_2 VARCHAR, variety VARCHAR, winery VARCHAR);')
+    #conn.commit()
+    f.close()
+
+    url = "jdbc:postgresql://172.17.0.3/wineDb?user=username&password=password"
+    df = (sparkSession.read.format("jdbc")
+        .options(url=url, dbtable="wine_reviews")
+        .load())
+
+    table = df.select('country','points').groupBy('country').agg(mean('points')).orderBy('avg(points)',ascending=False)
+    countryCols = table.select('country').collect()
+    countries = list()
+    for country in countryCols:
+        countries.append(str(country[0]))
+    pointCols = table.select('avg(points)').collect()
+    points = list()
+    for point in pointCols:
+        points.append(point[0])
+    data =  dict(type = 'choropleth',
+            locationmode='country names',
+            locations = countries,
+            colorscale='Blues',
+            z = points,
+            colorbar = {'title': 'Average Rating'}
+    )
+    layout = dict(geo = {'scope':'world'})
+    choromap = dict(data=[data], layout=layout)
+    reutrn map(choromap)
+
+
+
+
 def map(choromap):
     #get the html file path
     plot_url = plot(choromap, filename='map.html')
@@ -32,43 +69,6 @@ def map(choromap):
     os.rename('/opt/app-root/src/map.html', '/opt/app-root/src/templates/map.html')
     resp = render_template("map.html", title = 'Maps')
     return resp
-
-import psycopg2
-conn = psycopg2.connect("host='172.17.0.3' port='5432' dbname='wineDb' user='username' password='password'")
-cur = conn.cursor()
-#make table
-f = open(r'/opt/app-root/src/wineData.csv', 'r')
-cur.copy_from(f, "wine_reviews", sep=',')
-conn.commit()
-#cur.execute('create table wine_reviews(country VARCHAR, designation VARCHAR, points INT, price VARCHAR, province VARCHAR, region_1 VARCHAR, region_2 VARCHAR, variety VARCHAR, winery VARCHAR);')
-#conn.commit()
-f.close()
-
-url = "jdbc:postgresql://172.17.0.3/wineDb?user=username&password=password"
-df = (sparkSession.read.format("jdbc")
-    .options(url=url, dbtable="wine_reviews")
-    .load())
-
-table = df.select('country','points').groupBy('country').agg(mean('points')).orderBy('avg(points)',ascending=False)
-countryCols = table.select('country').collect()
-countries = list()
-for country in countryCols:
-    countries.append(str(country[0]))
-pointCols = table.select('avg(points)').collect()
-points = list()
-for point in pointCols:
-    points.append(point[0])
-data =  dict(type = 'choropleth',
-        locationmode='country names',
-        locations = countries,
-        colorscale='Blues',
-        z = points,
-        colorbar = {'title': 'Average Rating'}
-)
-layout = dict(geo = {'scope':'world'})
-choromap = dict(data=[data], layout=layout)
-map(choromap)
-
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
