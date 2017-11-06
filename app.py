07ae65a6c84ed2ef4612265ad54863871cebb2d8
rebeccaSimmonds19 <rmsimmonds1919@gmail.com>
2017-11-06 15:51:19 +0000

    typo

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def index():
      # make the templates dir
     newpath = r'/opt/app-root/src/templates'
     if not os.path.exists(newpath):
-        os.makedirs(newpath, 0o77)
+        os.makedirs(newpath, 077)
     # move the file to the templates dir
     os.rename('/opt/app-root/src/map.html', '/opt/app-root/src/templates/map.html')
     resp = render_template("map.html", title='Maps')
